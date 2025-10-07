b06e239181df3cc31b540ce4a7b95ee80d93441d
miguel <vianamiguelangelo@gmail.com>
2025-10-07 11:57:47 -0300

    ops

---
+++ myrep/poo/chinela/py/draft.py
@@ -0,0 +1,16 @@
+class Chinela: 
+    def __init__(self, ):
+        self.__tamanho =  0 
+        self.settamanho = 
+
+    def getTamanho(self):
+         return self.__tamanho    
+    
+    def setTamanho(self, valor : int):
+        #implementar os valores da chinela...
+
+
+chinela = Chinela() 
+
+while chinela.getTamanho() == 0 :
+    print("Digite seu tamanho de chinela")
