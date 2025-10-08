af07e7c8a00e7f16f7b314aa35d446ef8e270b2c
miguel <vianamiguelangelo@gmail.com>
2025-10-08 16:29:53 -0300

    mpls

--- myrep/poo/chinela/py/draft.py
+++ myrep/poo/chinela/py/draft.py
@@ -1,16 +1,22 @@
-class Chinela: 
-    def __init__(self, ):
-        self.__tamanho =  0 
-        self.settamanho = 
+class Slipper:
+    def __init__(self):
+        self.__tamanho : int = 0
 
-    def getTamanho(self):
-         return self.__tamanho    
-    
-    def setTamanho(self, valor : int):
-        #implementar os valores da chinela...
+    def get_size(self) -> int :
+          return self.__tamanho
+    def setTamnho(self, valor: int ):
+          if valor > 19 and valor < 51: 
+              self.__tamanho = valor 
+              return
+          else: 
+              print("fail: o valor digitado não corresponde a um par de chinela ")
+              return 
 
-
-chinela = Chinela() 
-
-while chinela.getTamanho() == 0 :
-    print("Digite seu tamanho de chinela")
+def main():
+     chinelin = Slipper()
+     while chinelin.get_size() == 0:
+          tamanho = int(input(": ")) 
+          chinelin.setTamnho(tamanho)
+     print("parabens, você comprou uma chinela de tamanho ", chinelin.get_size())
+main()     
+              
