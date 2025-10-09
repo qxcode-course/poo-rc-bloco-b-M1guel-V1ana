46cafe6b65fa5fb19f24d70ae229edc55c33377f
miguel <vianamiguelangelo@gmail.com>
2025-10-09 14:13:34 -0300

    avacalhou

---
+++ myrep/poo/roupa/py/draft.py
@@ -0,0 +1,8 @@
+class Roupa:
+    def __init__(self):
+        self.__tamanhoroupa : str = ""
+
+    def setTamanho(self):
+        return self.__tamanhoroupa    
+    def getTamanho(self, valor : str):
+        
