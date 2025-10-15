4d2e3838ce336cfb29a005ab5f7b40f6fb98f704
miguel <vianamiguelangelo@gmail.com>
2025-10-14 21:43:41 -0300

    atividade quase finalizada

--- myrep/poo/relogio/py/draft.py
+++ myrep/poo/relogio/py/draft.py
@@ -1,42 +1,54 @@
 class Watch:
-    def __init__(self, hora: int = 0 , minuto: int=0 , segundo: int=0):
-        self.__hora = hora 
-        self.__minuto = minuto 
-        self.__segundo =  segundo
+    def __init__(self, hora: int = 0, minuto: int = 0, segundo: int = 0):
+        self.__hora = 0
+        self.__minuto = 0
+        self.__segundo = 0
+        
+        self.setHoras(hora)
+        self.setMin(minuto)
+        self.setSeg(segundo)
     
     def getHoras(self):
-        return self.hora 
-    def getMinutos(self):
-        return self.minuto 
-    def getSegundos(self):
-        return self.segundo
-    
-    def setHoras(self, h : int):
-        if h >23 or h>0:
-            self.__hora = h
-            return
-        else:
-            print("horas invalidas")
-    def setMin(self, m: int):
-        if m > 0 or m < 60 :
-            self.__minuto  = m 
-            return
-        else:
-            print("minutos invalidos")
-    def setSeg(self, s : int):
-        if s > 0 or s < 60:
-            self.__segundo = s 
-            return
-        else:
-            print("segundos invalidos")        
+        return self.__hora 
 
+    def getMinutos(self):
+        return self.__minuto 
+
+    def getSegundos(self):
+        return self.__segundo
     
-            
+    def setHoras(self, h: int):
+        if h >= 0 and h <= 23:
+            self.__hora = h
+        else:
+            print("fail: hora invalida")
+        
+    def setMin(self, m: int):
+        if m >= 0 and m <=59:
+            self.__minuto = m
+        else:
+            print("fail: minuto invalido")
+
+    def setSeg(self, s: int):
+        if s >=0 and s<= 59 :
+            self.__segundo = s
+        else:
+            print("fail: segundo invalido")   
+        
+    def nextSecond(self):
+        self.__segundo += 1
+        if self.__segundo > 59:
+            self.__segundo = 0
+            self.__minuto += 1
+        if self.__minuto > 59:
+            self.__minuto = 0
+            self.__hora += 1
+        if self.__hora > 23:
+            self.__hora = 0
 
     def __str__(self):
         return f"{self.__hora:02d}:{self.__minuto:02d}:{self.__segundo:02d}"
     
-
 
 def main():
     relogin = Watch()
@@ -44,13 +56,28 @@ def main():
     while True:
         line = input()
         print("$" + line)
-        args = list[args] = line.split()
+        args = line.split()
 
         if args[0] == "end":
             break
+
         if args[0] == "show":
             print(relogin)
+
+        if args[0] == "init":
+            h, m, s = map(int, args[1:])
+            relogin.setHoras(h)
+            relogin.setMin(m)
+            relogin.setSeg(s)
+
         if args[0] == "set":
-            
+            h, m, s = map(int, args[1:])
+            relogin.setHoras(h)
+            relogin.setMin(m)
+            relogin.setSeg(s)
+
+        if args[0] == "next":
+            relogin.nextSecond()
+
 
 main()
