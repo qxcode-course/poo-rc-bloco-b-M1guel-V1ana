a3dc8ea056c2e64316051ffdb205a70387dce486
Miguel <miguel@gmail.com>
2025-10-16 09:51:58 -0300

    fazendo a motinha

--- myrep/poo/notebook/py/draft.py
+++ myrep/poo/notebook/py/draft.py
@@ -1,3 +1,34 @@
 class Notebook:
     def __init__(self):
-        smfksd
+        self.__ligar : bool = False
+    
+
+    def getLigar(self):
+        return self.__ligar
+    
+    def setLigar(self, valor : bool):
+        self.__ligar = valor 
+        return
+    
+    def Ligar(self):
+        if not self.__ligar:
+            self.ligar = True
+            print("notebook ligado.")
+        else:
+            print("notebook já está ligado.")
+
+    
+
+def main():
+    pc = Notebook()
+
+    while True:
+        line = input()
+        print("$" + line)
+        args: list[args] = line.split()
+
+        if args[0] == "end":
+            break
+        if args[0] == "mostrar":
+            print(pc)
+main()
